415398a00a5d11599b3882ea56fc97b857ac9d0e
xmhh <13720405602@163.com>
2022-11-25 14:03:57 +0800

    simple version

--- test_gif.py
+++ test_gif.py
@@ -5,6 +5,8 @@ files = os.listdir('temp')
 files = sorted(files, key=lambda file: os.path.getctime(os.path.join('temp', file)))
 frames = []
 for img in files:
-    frames.append(Image.open(os.path.join('temp', img)))
+    img = Image.open(os.path.join('temp', img))
+    img = img.resize((img.size[0]//2, img.size[1]//2), Image.CUBIC)
+    frames.append(img)
 
 frames[0].save('temp.gif', save_all = True, append_images = frames[1:], duration =800, loop=10)
